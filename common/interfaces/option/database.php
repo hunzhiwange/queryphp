b458bec0c65106ccff1554b1b93e778a9b676df0
Xiangmin Liu <l>
2017-05-24 20:45:51 +0800

    环境变量优化完毕

--- common/interfaces/option/database.php
+++ common/interfaces/option/database.php
@@ -3,15 +3,15 @@
  * 默认配置文件
  */
 return [    
-        'host' => 'localhost', // 数据库地址
-        'user' => 'root', // 数据库用户名
-        'password' => '', // 数据库密码
-        'prefix' => 'zt_', // 数据库表前缀
+       // 'host' => 'localhost', // 数据库地址
+       // 'user' => 'root', // 数据库用户名
+       /// 'password' => '', // 数据库密码
+        //'prefix' => 'zt_', // 数据库表前缀
         'char' => 'utf8', // 数据库编码
-        'name' => 'candao_project', // 数据库名字
+        //'name' => 'candao_project', // 数据库名字
         'type' => 'mysql', // 数据库类型
 //         'schema' => '', // 数据库SCHEMA
-         'port' => 3306, // 端口
+//         'port' => 3306, // 端口
 //         'cache' => FALSE, // 数据库查询是否缓存
 //         'meta_cached' => TRUE, // 数据库元是否缓存
 //         'distributed' => FALSE, // 是否采用分布式
