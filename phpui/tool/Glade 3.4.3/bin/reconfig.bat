1972015fcf6b60ea2e16c42228424ee0be53c352
Xiangmin Liu <l>
2017-11-09 14:22:57 +0800

    remove phpui

--- phpui/tool/Glade 3.4.3/bin/reconfig.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-gdk-pixbuf-query-loaders.exe > ..\etc\gtk-2.0\gdk-pixbuf.loaders
-gtk-query-immodules-2.0.exe > ..\etc\gtk-2.0\gtk.immodules
-pango-querymodules.exe > ..\etc\pango\pango.modules
