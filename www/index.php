79ca8ee17eff68a33062ed8ddc2570cae36413e7
Xiangmin Liu <635750556@qq.com>
2018-08-26 23:13:18 +0800

    improvoing composer

--- www/index.php
+++ www/index.php
@@ -27,26 +27,28 @@ use Leevel\Kernel\Runtime\IRuntime;
  * 用于管理 PHP 依赖包
  * 优化 composer 性能，优先载入 composer 注册的 Psr4
  * 如果 Psr4 不存在，才会去初始化 composer 加载，使得大部分请求不走 composer 的自动载入
- * 如果你使用的包是比较新的，基本都遵循 Psr4 规则，这个时候会提升 30~40 ms 上下性能
+ * 如果你使用的包是比较新的，基本都遵循 Psr4 规则，这个时候会提升一部分性能
+ * 对于助手函数需要自己引入
  */
-/*$psr4s = include_once __DIR__ . '/../vendor/composer/autoload_psr4.php';
+$psr4s = include_once __DIR__.'/../vendor/composer/autoload_psr4.php';
 
-require_once  __DIR__ . '/../vendor/hunzhiwange/framework/src/Queryyetsimple/Bootstrap/function.php';
+require_once __DIR__.'/../vendor/hunzhiwange/framework/src/Queryyetsimple/Bootstrap/function.php';
 
-spl_autoload_register(function ($className) use($psr4s) {
+spl_autoload_register(function ($className) use ($psr4s) {
     static $loadedComposer;
 
     $name = explode('\\', $className);
 
     // 顶层命名空间
-    $topLevel = $name[0] . '\\';
+    $topLevel = $name[0].'\\';
 
     if (isset($psr4s[$topLevel])) {
         foreach ($psr4s[$topLevel] as $dir) {
-            $file =  $dir . '/' . str_replace('\\', '/', substr($className, strlen($topLevel))) . '.php';
+            $file = $dir.'/'.str_replace('\\', '/', substr($className, strlen($topLevel))).'.php';
 
             if (is_file($file)) {
                 require_once $file;
+
                 break;
             }
         }
@@ -58,11 +60,12 @@ spl_autoload_register(function ($className) use($psr4s) {
 
     // 第三层 todo
 
-    if (! $loadedComposer) {*/
-       require_once __DIR__.'/../vendor/autoload.php';
-       /*$loadedComposer = true;
+    if (!$loadedComposer) {
+        $composer = require_once __DIR__.'/../vendor/autoload.php';
+        $composer->loadClass($className);
+        $loadedComposer = true;
     }
-});*/
+});
 
 /**
  * ---------------------------------------------------------------
