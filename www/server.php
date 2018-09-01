c6d8ee4805b9ad102c147f26adefbafd7d2b537f
Xiangmin Liu <635750556@qq.com>
2018-09-01 10:57:23 +0800

    add server.php

---
+++ www/server.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of the forcodepoem package.
+ *
+ * The PHP Application Created By Code Poem. <Query Yet Simple>
+ * (c) 2018-2099 http://forcodepoem.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__.'/index.php';
