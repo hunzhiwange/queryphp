2ecec4d10f59efd284e0f61ba36e0afd76e5d7d5
Xiangmin Liu <635750556@qq.com>
2018-09-26 22:41:47 +0800

    fix index.php for styleci

--- www/index.php
+++ www/index.php
@@ -29,7 +29,9 @@ use Leevel\Kernel\Runtime\IRuntime;
  * 我们 classmap 需要通过 `php leevel autoload` 生成，包含命令 `composer dump-autoload -o`
  * 对于助手函数需要自己引入
  */
-if (is_file($leevelAutoload = __DIR__.'/../runtime/autoload.php')) {
+$leevelAutoload = __DIR__.'/../runtime/autoload.php';
+
+if (is_file($leevelAutoload)) {
     require_once $leevelAutoload;
 } else {
     require_once __DIR__.'/../vendor/autoload.php';
