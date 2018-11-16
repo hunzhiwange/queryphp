964d1ca50c627e3e2d4bdf844a3c77a9ffea15e3
Xiangmin Liu <635750556@qq.com>
2018-11-16 09:04:48 +0800

    frontend config remove from git

--- frontend/config/dev.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    NODE_ENV: '"development"',
-    ENV_CONFIG: '"dev"',
-    BASE_API: '"http://queryphp.cn"',
-    APP_ID: '"admin"',
-    APP_KEY: '"adminxxxxxxxxxxxx"'
-}
--- frontend/config/prod.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    NODE_ENV: '"production"',
-    ENV_CONFIG: '"prod"',
-    BASE_API: '"http://queryphp.cn"',
-    APP_ID: '"admin"',
-    APP_KEY: '"adminxxxxxxxxxxxx"'
-};
