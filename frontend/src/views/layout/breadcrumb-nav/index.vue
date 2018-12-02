b32fa64db0952cc3efd2cbe05c63c547309a01e6
Xiangmin Liu <635750556@qq.com>
2018-12-02 18:08:28 +0800

    add profile

--- frontend/src/views/layout/breadcrumb-nav/index.vue
+++ frontend/src/views/layout/breadcrumb-nav/index.vue
@@ -1,7 +1,7 @@
 <template>
     <Breadcrumb separator="<b class='breadcrumb-separator'>></b>">
-        <BreadcrumbItem v-for="item in currentPath" :key="item.name"
-            ><Icon :type="itemIcon(item)" :class="itemIcon(item)"></Icon>
+        <BreadcrumbItem v-for="item in currentPath" :key="item.name">
+            <Icon v-if="item.icon" :type="itemIcon(item)" :class="itemIcon(item)"></Icon>
             {{ itemTitle(item) }}</BreadcrumbItem
         >
     </Breadcrumb>
