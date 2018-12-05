91e27df916b4de6b27a0541248e910751640ee1e
Xiangmin Liu <635750556@qq.com>
2018-12-05 21:38:21 +0800

    rebuild frontend

--- frontend/src/views/layout/fullscreen/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div @click="handleChange" v-if="showFullScreenBtn">
-        <Icon :type="value ? 'arrow-shrink' : 'arrow-expand'"></Icon>
-        {{ value ? __('退出全屏') : __('全屏') }}
-    </div>
-</template>
-
-<script src="./assets/index.js"></script>
--- frontend/src/views/layout/lockscreen/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div @click="lockScreen"><Icon type="locked"></Icon> {{ __('锁屏') }}</div>
-</template>
-
-<script src="./assets/index.js"></script>
