60296784397354014ab9fbbef0c4f48356c2fa48
Xiangmin Liu <635750556@qq.com>
2018-12-05 22:11:07 +0800

    add prettier

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+    bracketSpacing: false,
+    singleQuote: true,
+    jsxBracketSameLine: true,
+    trailingComma: 'es5',
+    printWidth: 160,
+    semi: false,
+}
