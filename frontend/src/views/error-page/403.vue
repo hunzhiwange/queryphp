5d08e2ec58d7246f377fa5d1d705f67f7be49f0f
Xiangmin Liu <635750556@qq.com>
2018-12-09 14:55:06 +0800

    update icon

--- frontend/src/views/error-page/403.vue
+++ frontend/src/views/error-page/403.vue
@@ -3,30 +3,13 @@
         <div class="error403-body-con">
             <Card>
                 <div class="error403-body-con-title">
-                    4<span class="error403-0-span"
-                        ><Icon type="android-lock"></Icon></span
-                    ><span class="error403-key-span"
-                        ><Icon size="220" type="ios-bolt"></Icon
-                    ></span>
+                    4<span class="error403-0-span"><Icon type="android-lock"></Icon></span
+                    ><span class="error403-key-span"><Icon size="220" type="ios-leaf"></Icon></span>
                 </div>
-                <p class="error403-body-con-message">
-                    You don't have permission
-                </p>
+                <p class="error403-body-con-message">You don't have permission</p>
                 <div class="error403-btn-con">
-                    <i-button
-                        @click="goHome"
-                        size="large"
-                        style="width: 200px;"
-                        type="text"
-                        >{{ __('返回首页') }}</i-button
-                    >
-                    <i-button
-                        @click="backPage"
-                        size="large"
-                        style="width: 200px;margin-left: 40px;"
-                        type="primary"
-                        >{{ __('返回上一页') }}</i-button
-                    >
+                    <i-button @click="goHome" size="large" style="width: 200px;" type="text">{{ __('返回首页') }}</i-button>
+                    <i-button @click="backPage" size="large" style="width: 200px;margin-left: 40px;" type="primary">{{ __('返回上一页') }}</i-button>
                 </div>
             </Card>
         </div>
