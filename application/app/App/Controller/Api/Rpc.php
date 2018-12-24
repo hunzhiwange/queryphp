f191933ab0df575f2f3a1271419342d21d0b42d4
Xiangmin Liu <635750556@qq.com>
2018-12-24 21:20:06 +0800

    add rpc for swoole

---
+++ application/app/App/Controller/Api/Rpc.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the forcodepoem package.
+ *
+ * The PHP Application Created By Code Poem. <Query Yet Simple>
+ * (c) 2018-2099 http://forcodepoem.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\App\Controller\Api;
+
+use Leevel\Http\IResponse;
+use Leevel\Protocol\Facade\Rpc as Rpcs;
+
+/**
+ * rpc tests.
+ *
+ * @author Name Your <your@mail.com>
+ *
+ * @since 2018.08.31
+ *
+ * @version 1.0
+ */
+class Rpc
+{
+    /**
+     * 默认方法.
+     *
+     * @return \Leevel\Http\IResponse
+     */
+    public function handle(): IResponse
+    {
+        return Rpcs::call('api/rpc/rpc-result', ['foo', 'bar'], ['arg1' => 'hello', 'arg2' => 'world']);
+    }
+
+    /**
+     * RPC 结果.
+     *
+     * @return array
+     */
+    public function rpcResult(string $arg1, string $arg2, array $metas): array
+    {
+        return ['arg1' => $arg1, 'arg2' => $arg2, 'metas' => $metas];
+    }
+}
