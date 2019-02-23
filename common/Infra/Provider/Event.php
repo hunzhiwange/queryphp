b833c765367c6233e1a3852ac5ee59625585275b
Xiangmin Liu <635750556@qq.com>
2019-02-23 14:51:02 +0800

    fix<provider>: fix router privider

--- common/Infra/Provider/Event.php
+++ common/Infra/Provider/Event.php
@@ -28,20 +28,20 @@ use Leevel\Event\EventProvider;
 class Event extends EventProvider
 {
     protected $listeners = [
-        'Common\Domain\Event\Test' => [
+        'Common\\Domain\\Event\\Test' => [
             // 优先级支持写法，数字越小越早执行，默认为 500
-            // 'Common\Domain\Listener\Test' => 6
-            'Common\Domain\Listener\Test',
-            'Common\Domain\Listener\Test2' => 5,
-            'Common\Domain\Listener\Test3' => 2,
+            // 'Common\\Domain\\Listener\\Test' => 6
+            'Common\\Domain\\Listener\\Test',
+            'Common\\Domain\\Listener\\Test2' => 5,
+            'Common\\Domain\\Listener\\Test3' => 2,
         ],
 
-        'Common\Domain\Event\Wildcards*' => [
-            'Common\Domain\Listener\Test',
+        'Common\\Domain\\Event\\Wildcards*' => [
+            'Common\\Domain\\Listener\\Test',
         ],
 
-        'Common\Domain\Event\WildcardsTest' => [
-            'Common\Domain\Listener\Test2',
+        'Common\\Domain\\Event\\WildcardsTest' => [
+            'Common\\Domain\\Listener\\Test2',
         ],
     ];
 }
