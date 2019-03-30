2de7dccc912090857b485efc59ce459a89b27ed8
Xiangmin Liu <635750556@qq.com>
2019-03-30 20:05:57 +0800

    refactor: opimization the admin auth middleware

--- application/admin/App/Middleware/Auth.php
+++ application/admin/App/Middleware/Auth.php
@@ -36,6 +36,42 @@ use Leevel\Kernel\UnauthorizedHttpException;
 class Auth extends BaseAuth
 {
     /**
+     * 忽略锁定路由.
+     *
+     * @return array
+     */
+    private $ignoreLockPathInfo = [
+        '/:admin/user/lock',
+        '/:admin/user/unlock',
+        '/:admin/login/logout',
+    ];
+
+    /**
+     * 忽略路由.
+     *
+     * @return array
+     */
+    private $ignorePathInfo = [
+        '/:admin/login/code',
+        '/:admin/login/validate',
+    ];
+
+    /**
+     * 忽略权限路由.
+     *
+     * @return array
+     */
+    private $ignorePermissionPathInfo = [
+        '/:admin/login/logout',
+        '/:admin/user/update-info',
+        '/:admin/user/lock',
+        '/:admin/user/unlock',
+        '/:admin/user/change-password',
+        '/:admin/user/permission',
+        '/:admin/search',
+    ];
+
+    /**
      * 请求
      *
      * @param \Closure              $next
@@ -43,40 +79,21 @@ class Auth extends BaseAuth
      */
     public function handle(Closure $next, IRequest $request): void
     {
-        // header('Access-Control-Allow-Origin: '.($_SERVER['HTTP_ORIGIN'] ?? ''));
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token');
+        $this->prepareCors($request);
 
-        // 跨域校验
-        if ($request->isOptions()) {
-            echo 'Die because of CORS';
-            die;
-        }
-
-        if (\in_array($pathInfo = $request->getPathInfo(), $this->ignorePathInfo(), true)) {
+        if ($this->isIgnoreRouter($request)) {
             $next($request);
 
             return;
         }
 
-        // 兼容 header，也可以通过 get 或者 post 来设置 token
-        if ($token = $request->headers->get('token')) {
-            $request->query->set('token', $token);
-        } else {
-            $token = $request->query->get('token');
-        }
+        $token = $this->normalizeToken($request);
 
         try {
             if ($this->manager->isLogin()) {
-                // 后台已锁定
-                if (!\in_array($pathInfo, $this->ignoreLockPathInfo(), true) &&
-                    $token && (new Lock())->has($token)) {
-                    throw new LockException(__('系统已锁定'));
-                }
+                $this->validateLock($request, $token);
 
-                if (!\in_array($pathInfo, $this->ignorePermissionPathInfo(), true)) {
+                if (!$this->isIgnorePermission($request)) {
                     $this->validatePermission($request);
                 }
             }
@@ -88,11 +105,87 @@ class Auth extends BaseAuth
     }
 
     /**
+     * 准备跨域数据.
+     *
+     * @param \Leevel\Http\IRequest $request
+     */
+    private function prepareCors(IRequest $request): void
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token');
+
+        // 跨域校验
+        if ($request->isOptions()) {
+            echo 'Die because of CORS';
+            die;
+        }
+    }
+
+    /**
+     * 是否为忽略路由.
+     *
+     * @param \Leevel\Http\IRequest $request
+     *
+     * @return bool
+     */
+    private function isIgnoreRouter(IRequest $request): bool
+    {
+        return \in_array($request->getPathInfo(), $this->ignorePathInfo, true);
+    }
+
+    /**
+     * 整理 token 数据.
+     *
+     * @param \Leevel\Http\IRequest $request
+     *
+     * @return string
+     */
+    private function normalizeToken(IRequest $request): string
+    {
+        // 兼容 header，也可以通过 get 或者 post 来设置 token
+        if ($token = $request->headers->get('token')) {
+            $request->query->set('token', $token);
+        } else {
+            $token = $request->query->get('token', '');
+        }
+
+        return $token;
+    }
+
+    /**
+     * 验证是否锁定.
+     *
+     * @param \Leevel\Http\IRequest $request
+     * @param string                $token
+     */
+    private function validateLock(IRequest $request, string $token): void
+    {
+        if (!\in_array($request->getPathInfo(), $this->ignoreLockPathInfo, true) &&
+            $token && (new Lock())->has($token)) {
+            throw new LockException(__('系统已锁定'));
+        }
+    }
+
+    /**
+     * 是否为忽略权限.
+     *
+     * @param \Leevel\Http\IRequest $request
+     *
+     * @return bool
+     */
+    private function isIgnorePermission(IRequest $request): bool
+    {
+        return \in_array($request->getPathInfo(), $this->ignorePermissionPathInfo, true);
+    }
+
+    /**
      * 权限校验.
      *
      * @param \Leevel\Http\IRequest $request
      */
-    protected function validatePermission(IRequest $request): void
+    private function validatePermission(IRequest $request): void
     {
         $pathInfo = str_replace('/:admin/', '', $request->getPathInfo());
         $method = strtolower($request->getMethod());
@@ -100,50 +193,5 @@ class Auth extends BaseAuth
         if (!Permission::handle($pathInfo, $method)) {
             throw new HandleException('你没有权限执行操作');
         }
-    }
-
-    /**
-     * 忽略路由.
-     *
-     * @return array
-     */
-    protected function ignorePathInfo(): array
-    {
-        return [
-            '/:admin/login/code',
-            '/:admin/login/validate',
-        ];
-    }
-
-    /**
-     * 忽略权限路由.
-     *
-     * @return array
-     */
-    protected function ignorePermissionPathInfo(): array
-    {
-        return [
-            '/:admin/login/logout',
-            '/:admin/user/update-info',
-            '/:admin/user/lock',
-            '/:admin/user/unlock',
-            '/:admin/user/change-password',
-            '/:admin/user/permission',
-            '/:admin/search',
-        ];
-    }
-
-    /**
-     * 忽略锁定路由.
-     *
-     * @return array
-     */
-    protected function ignoreLockPathInfo(): array
-    {
-        return [
-            '/:admin/user/lock',
-            '/:admin/user/unlock',
-            '/:admin/login/logout',
-        ];
     }
 }
