660c5c5a6289e1b13f6ea1a96dc7be14cff7753a
XiangMin Liu <635750556@qq.com>
2019-05-30 14:04:52 +0800

    chore(phpstan): add phpstan

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the your app package.
+ *
+ * The PHP Application For Code Poem For You.
+ * (c) 2018-2099 http://yourdomian.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * phpstan 静态检查启动文件.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.05.30
+ *
+ * @version 1.0
+ */
+include __DIR__.'/tests/bootstrap.php'; /** @codeCoverageIgnore */
+
+/**
+ * 导入助手函数.
+ *
+ * @codeCoverageIgnoreStart
+ */
+$fnDirs = [
+    __DIR__.'/common/Infra/Helper',
+];
+
+foreach ($fnDirs as $dir) {
+    $files = glob($dir.'/*.php');
+
+    foreach ($files as $file) {
+        include_once $file;
+    }
+}
+// @codeCoverageIgnoreEnd
