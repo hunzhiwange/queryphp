fc69d4bf7c5eaf9235f1b2c309bcfd206f006b5c
XiangMin Liu <635750556@qq.com>
2019-07-01 21:12:30 +0800

    feat: add demo for swoole_timer_tick

---
+++ application/app/App/Controller/Timer/Work.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the your app package.
+ *
+ * The PHP Application For Code Poem For You.
+ * (c) 2018-2099 http://yourdomian.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\App\Controller\Timer;
+
+use Closure;
+use Common\Infra\Helper\message_with_time;
+use Exception;
+use Throwable;
+
+/**
+ * 定时器工作任务.
+ *
+ * @author Name Your <your@mail.com>
+ *
+ * @since 2019.07.01
+ *
+ * @version 1.0
+ */
+class Work
+{
+    /**
+     * 执行入口.
+     *
+     * @return string
+     */
+    public function handle(): string
+    {
+        $this->work(function (): void {
+            $this->message('Failed test');
+
+            throw new Exception('Failed test');
+        }, 5 * 1000, 5);
+
+        $this->work(function (): void {
+            $this->message('Success test');
+        }, 5 * 1000, 5);
+
+        return 'Work done';
+    }
+
+    /**
+     * 执行任务
+     *
+     * @param \Closure $work
+     * @param int      $perMillisecond
+     * @param int      $maxCount
+     */
+    private function work(Closure $work, int $perMillisecond, int $maxCount): void
+    {
+        $count = 0;
+
+        swoole_timer_tick($perMillisecond, function ($timerId) use ($work, &$count, $perMillisecond, $maxCount) {
+            $count++;
+            $this->message(sprintf('Work doing at `%d` try', $count));
+
+            try {
+                $work();
+                swoole_timer_clear($timerId);
+                $this->message(sprintf('Work successed at `%d` try', $count));
+            } catch (Throwable $th) {
+                if ($count >= $maxCount) {
+                    swoole_timer_clear($timerId);
+                    $this->message(sprintf('Work faield after `%d` tries', $maxCount));
+                } else {
+                    $this->message(sprintf('Try after `%d` millisecond', $perMillisecond));
+                }
+            }
+        });
+    }
+
+    /**
+     * 输出消息.
+     *
+     * @param string $message
+     */
+    private function message(string $message): void
+    {
+        dump(f(message_with_time::class, $message));
+    }
+}
---
+++ common/Infra/Helper/message_with_time.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the your app package.
+ *
+ * The PHP Application For Code Poem For You.
+ * (c) 2018-2099 http://yourdomian.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Common\Infra\Helper;
+
+/**
+ * 带有时间的消息.
+ *
+ * ``` php
+ * f('Common\\Infra\\Helper\\message_with_time');
+ * ```
+ *
+ * @param string $message
+ * @param string $format
+ */
+function message_with_time(string $message, string $format = 'Y-m-d H:i:s'): string
+{
+    return sprintf('[%s]', date($format)).$message.PHP_EOL;
+}
+
+class message_with_time
+{
+}
