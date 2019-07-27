3117cfca76fa3703b5008f16f5488e7109ed8015
XiangMin Liu <635750556@qq.com>
2019-07-27 15:42:21 +0800

    feat: add database coroutine

---
+++ application/app/App/Controller/Coroutine/Database.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the your app package.
+ *
+ * The PHP Application For Code Poem For You.
+ * (c) 2018-2099 http://yourdomian.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\App\Controller\Coroutine;
+
+use Common\Infra\Helper\message_with_time;
+
+/**
+ * 数据库.
+ *
+ * @author Name Your <your@mail.com>
+ *
+ * @since 2019.07.27
+ *
+ * @version 1.0
+ */
+class Database
+{
+    /**
+     * 响应.
+     *
+     * @return string
+     */
+    public function handle(): string
+    {
+        $this->message('Start');
+
+        go(function () {
+            $time = time();
+            $db = app('database');
+
+            for ($i = 0; $i < 5; $i++) {
+                $result = app('database')->query('SELECT sleep(2)');
+                dump($result);
+            }
+
+            $this->message('Time: '.(time() - $time));
+        });
+
+        $this->message('Done');
+
+        return 'Done';
+    }
+
+    /**
+     * 输出消息.
+     *
+     * @param string $message
+     */
+    private function message(string $message): void
+    {
+        dump(f(message_with_time::class, $message));
+    }
+}
