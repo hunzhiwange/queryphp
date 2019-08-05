7703d730bef68416a5d83bc061c41332754f017a
XiangMin Liu <635750556@qq.com>
2019-08-05 08:57:37 +0800

    feat: add restful index

---
+++ application/app/App/Controller/Restful/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the your app package.
+ *
+ * The PHP Application For Code Poem For You.
+ * (c) 2018-2099 http://yourdomian.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\App\Controller\Restful;
+
+use Leevel\Router\IRouter;
+
+/**
+ * index.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.08.05
+ *
+ * @version 1.0
+ */
+class Index
+{
+    public function handle()
+    {
+        return 'hello for restful '.IRouter::RESTFUL_INDEX;
+    }
+}
