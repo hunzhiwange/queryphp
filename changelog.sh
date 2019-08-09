1d10414c549eee331be2a945c79e6d73536f3c28
XiangMin Liu <635750556@qq.com>
2019-08-09 09:17:28 +0800

    build(changelog): use commitizen and conventional-changelog tools

--- changelog.sh
+++ changelog.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+# https://cattail.me/tech/2016/06/06/git-commit-message-and-branching-model.html
+# https://www.jianshu.com/p/00c9ec4e552e
+# https://www.codercto.com/a/34733.html
+#
 # cnpm install -g commitizen
 # cnpm install -g conventional-changelog-cli
 # commitizen init cz-conventional-changelog --save --force
