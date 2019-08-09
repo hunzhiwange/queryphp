31b0879a594271e17f8dde3e213caf2f887e53bd
XiangMin Liu <635750556@qq.com>
2019-08-09 08:46:33 +0800

    build(changelog): use commitizen and conventional-changelog tools to generate changelogs

---
+++ changelog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# cnpm install -g commitizen
+# cnpm install -g conventional-changelog-cli
+# commitizen init cz-conventional-changelog --save --force
+# conventional-changelog -i CHANGELOG.md -s <npm run changelog>
+
+npm run changelog
