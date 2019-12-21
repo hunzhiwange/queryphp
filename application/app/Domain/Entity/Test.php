8e7e586e158f3d0b7b24eb990eb80ff7b34cb469
XiangMin Liu <635750556@qq.com>
2019-12-21 16:50:44 +0800

    fix(any): update new entity

--- application/app/Domain/Entity/Test.php
+++ application/app/Domain/Entity/Test.php
@@ -46,26 +46,26 @@ class Test extends Entity
      * Entity struct.
      *
      * - id
-     *                   comment: 'ID', type: 'int(11) unsigned', null: false,
-     *                   key: 'PRI', default: NULL, extra: 'auto_increment'
+     *                   comment: ID  type: int(11) unsigned  null: false
+     *                   key: PRI  default: NULL  extra: auto_increment
      * - name
-     *                   comment: '测试名', type: 'varchar(200)', null: false,
-     *                   key: '', default: '', extra: ''
+     *                   comment: 测试名  type: varchar(200)  null: false
+     *                   key:   default:   extra:
      * - create_at
-     *                   comment: '创建时间', type: 'datetime', null: false,
-     *                   key: '', default: 'CURRENT_TIMESTAMP', extra: ''
+     *                   comment: 创建时间  type: datetime  null: false
+     *                   key:   default: CURRENT_TIMESTAMP  extra:
      * - update_at
-     *                   comment: '更新时间', type: 'datetime', null: false,
-     *                   key: '', default: 'CURRENT_TIMESTAMP', extra: 'on update CURRENT_TIMESTAMP'
+     *                   comment: 更新时间  type: datetime  null: false
+     *                   key:   default: CURRENT_TIMESTAMP  extra: on update CURRENT_TIMESTAMP
      * - delete_at
-     *                   comment: '删除时间 0=未删除;大于0=删除时间;', type: 'bigint(20) unsigned', null: false,
-     *                   key: 'MUL', default: '0', extra: ''
+     *                   comment: 删除时间 0=未删除;大于0=删除时间;  type: bigint(20) unsigned  null: false
+     *                   key: MUL  default: 0  extra:
      * - create_account
-     *                   comment: '创建账号', type: 'int(11) unsigned', null: false,
-     *                   key: '', default: '0', extra: ''
+     *                   comment: 创建账号  type: int(11) unsigned  null: false
+     *                   key:   default: 0  extra:
      * - update_account
-     *                   comment: '更新账号', type: 'int(11) unsigned', null: false,
-     *                   key: '', default: '0', extra: ''
+     *                   comment: 更新账号  type: int(11) unsigned  null: false
+     *                   key:   default: 0  extra:
      *
      * @var array
      */
