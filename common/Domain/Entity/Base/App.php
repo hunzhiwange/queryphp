d825caa004c42372b7f301998bf62336673daa23
XiangMin Liu <635750556@qq.com>
2019-12-21 23:22:09 +0800

    fix(entity): update base entity

--- common/Domain/Entity/Base/App.php
+++ common/Domain/Entity/Base/App.php
@@ -15,36 +15,66 @@ declare(strict_types=1);
 namespace Common\Domain\Entity\Base;
 
 use Leevel\Database\Ddd\Entity;
-use Leevel\Database\Ddd\IEntity;
 
 /**
- * 应用实体.
+ * 应用.
  */
 class App extends Entity
 {
     /**
-     * table.
+     * Database table.
      *
      * @var string
      */
     const TABLE = 'app';
 
     /**
-     * id.
+     * Primary key.
      *
      * @var string
      */
     const ID = 'id';
 
     /**
-     * auto.
+     * Auto increment.
      *
-     * @var string
+     * @var null
      */
-    const AUTO = 'id';
+    const AUTO = null;
 
     /**
-     * struct.
+     * Entity struct.
+     *
+     * - id
+     *                   comment: ID  type: int(11) unsigned  null: false
+     *                   key: PRI  default: NULL  extra: auto_increment
+     * - num
+     *                   comment: 应用 ID  type: varchar(64)  null: false
+     *                   key: MUL  default:   extra:
+     * - key
+     *                   comment: 应用 KEY  type: varchar(64)  null: false
+     *                   key:   default:   extra:
+     * - secret
+     *                   comment: 应用秘钥  type: varchar(64)  null: false
+     *                   key:   default:   extra:
+     * - status
+     *                   comment: 状态 0=禁用;1=启用;  type: tinyint(4) unsigned  null: false
+     *                   key:   default: 1  extra:
+     * - create_at
+     *                   comment: 创建时间  type: datetime  null: false
+     *                   key:   default: CURRENT_TIMESTAMP  extra:
+     * - update_at
+     *                   comment: 更新时间  type: datetime  null: false
+     *                   key:   default: CURRENT_TIMESTAMP  extra: on update CURRENT_TIMESTAMP
+     * - delete_at
+     *                   comment: 删除时间 0=未删除;大于0=删除时间;  type: bigint(20) unsigned  null: false
+     *                   key:   default: 0  extra:
+     * - create_account
+     *                   comment: 创建账号  type: int(11) unsigned  null: false
+     *                   key:   default: 0  extra:
+     * - update_account
+     *                   comment: 更新账号  type: int(11) unsigned  null: false
+     *                   key:   default: 0  extra:
      *
      * @var array
      */
@@ -52,11 +82,16 @@ class App extends Entity
         'id' => [
             self::READONLY => true,
         ],
-        'num'       => [],
-        'status'    => [],
-        'key'       => [],
-        'secret'    => [],
-        'create_at' => [],
+        'num' => [
+        ],
+        'key' => [
+        ],
+        'secret' => [
+        ],
+        'status' => [
+        ],
+        'create_at' => [
+        ],
         'update_at' => [
             self::SHOW_PROP_BLACK => true,
         ],
@@ -72,7 +107,7 @@ class App extends Entity
     ];
 
     /**
-     * soft delete column.
+     * Soft delete column.
      *
      * @var string
      */
@@ -89,121 +124,56 @@ class App extends Entity
     ];
 
     /**
-     * database connect.
+     * Prop data.
+     *
+     * @var array
+     */
+    private array $data = [];
+
+    /**
+     * Database connect.
      *
      * @var mixed
      */
-    private static $leevelConnect;
+    private static $connect;
 
     /**
-     * id.
-     *
-     * @var int
-     */
-    private $id;
-
-    /**
-     * name.
-     *
-     * @var string
-     */
-    private $num;
-
-    /**
-     * 状态 0=禁用;1=启用;.
-     *
-     * @var int
-     */
-    private $status;
-
-    /**
-     * key.
-     *
-     * @var string
-     */
-    private $key;
-
-    /**
-     * secret.
-     *
-     * @var string
-     */
-    private $secret;
-
-    /**
-     * create_at.
-     *
-     * @var string
-     */
-    private $createAt;
-
-    /**
-     * 更新时间.
-     *
-     * @var string
-     */
-    private $updateAt;
-
-    /**
-     * 删除时间 0=未删除;大于0=删除时间;.
-     *
-     * @var int
-     */
-    private $deleteAt;
-
-    /**
-     * 创建账号.
-     *
-     * @var int
-     */
-    private $createAccount;
-
-    /**
-     * 更新账号.
-     *
-     * @var int
-     */
-    private $updateAccount;
-
-    /**
-     * setter.
+     * Setter.
      *
      * @param mixed $value
-     *
-     * @return $this
      */
-    public function setter(string $prop, $value): IEntity
+    public function setter(string $prop, $value): self
     {
-        $this->{$this->realProp($prop)} = $value;
+        $this->data[$this->realProp($prop)] = $value;
 
         return $this;
     }
 
     /**
-     * getter.
+     * Getter.
      *
      * @return mixed
      */
     public function getter(string $prop)
     {
-        return $this->{$this->realProp($prop)};
+        return $this->data[$this->realProp($prop)] ?? null;
     }
 
     /**
-     * set database connect.
+     * Set database connect.
      *
      * @param mixed $connect
      */
     public static function withConnect($connect): void
     {
-        static::$leevelConnect = $connect;
+        static::$connect = $connect;
     }
 
     /**
-     * get database connect.
+     * Get database connect.
      */
     public static function connect()
     {
-        return static::$leevelConnect;
+        return static::$connect;
     }
 }
--- common/Domain/Entity/Base/Option.php
+++ common/Domain/Entity/Base/Option.php
@@ -15,36 +15,60 @@ declare(strict_types=1);
 namespace Common\Domain\Entity\Base;
 
 use Leevel\Database\Ddd\Entity;
-use Leevel\Database\Ddd\IEntity;
 
 /**
- * Option.
+ * 系统配置.
  */
 class Option extends Entity
 {
     /**
-     * database table.
+     * Database table.
      *
      * @var string
      */
     const TABLE = 'option';
 
     /**
-     * primary key.
+     * Primary key.
      *
      * @var string
      */
     const ID = 'id';
 
     /**
-     * auto increment.
+     * Auto increment.
      *
-     * @var string
+     * @var null
      */
-    const AUTO = 'id';
+    const AUTO = null;
 
     /**
-     * entity struct.
+     * Entity struct.
+     *
+     * - id
+     *                   comment: ID  type: int(11) unsigned  null: false
+     *                   key: PRI  default: NULL  extra: auto_increment
+     * - name
+     *                   comment: 配置名  type: varchar(200)  null: false
+     *                   key: MUL  default:   extra:
+     * - value
+     *                   comment: 配置值  type: text  null: false
+     *                   key:   default: NULL  extra:
+     * - create_at
+     *                   comment: 创建时间  type: datetime  null: false
+     *                   key:   default: CURRENT_TIMESTAMP  extra:
+     * - update_at
+     *                   comment: 更新时间  type: datetime  null: false
+     *                   key:   default: CURRENT_TIMESTAMP  extra: on update CURRENT_TIMESTAMP
+     * - delete_at
+     *                   comment: 删除时间 0=未删除;大于0=删除时间;  type: bigint(20) unsigned  null: false
+     *                   key:   default: 0  extra:
+     * - create_account
+     *                   comment: 创建账号  type: int(11) unsigned  null: false
+     *                   key:   default: 0  extra:
+     * - update_account
+     *                   comment: 更新账号  type: int(11) unsigned  null: false
+     *                   key:   default: 0  extra:
      *
      * @var array
      */
@@ -52,8 +76,10 @@ class Option extends Entity
         'id' => [
             self::READONLY => true,
         ],
-        'name'      => [],
-        'value'     => [],
+        'name' => [
+        ],
+        'value' => [
+        ],
         'create_at' => [
             self::SHOW_PROP_BLACK => true,
         ],
@@ -72,114 +98,63 @@ class Option extends Entity
     ];
 
     /**
-     * soft delete column.
+     * Soft delete column.
      *
      * @var string
      */
     const DELETE_AT = 'delete_at';
 
     /**
-     * database connect.
+     * Prop data.
+     *
+     * @var array
+     */
+    private array $data = [];
+
+    /**
+     * Database connect.
      *
      * @var mixed
      */
-    private static $leevelConnect;
+    private static $connect;
 
     /**
-     * id.
-     *
-     * @var int
-     */
-    private $id;
-
-    /**
-     * 配置名.
-     *
-     * @var string
-     */
-    private $name;
-
-    /**
-     * 配置值.
-     *
-     * @var string
-     */
-    private $value;
-
-    /**
-     * 创建时间.
-     *
-     * @var string
-     */
-    private $createAt;
-
-    /**
-     * 更新时间.
-     *
-     * @var string
-     */
-    private $updateAt;
-
-    /**
-     * 删除时间 0=未删除;大于0=删除时间;.
-     *
-     * @var int
-     */
-    private $deleteAt;
-
-    /**
-     * 创建账号.
-     *
-     * @var int
-     */
-    private $createAccount;
-
-    /**
-     * 更新账号.
-     *
-     * @var int
-     */
-    private $updateAccount;
-
-    /**
-     * setter.
+     * Setter.
      *
      * @param mixed $value
-     *
-     * @return $this
      */
-    public function setter(string $prop, $value): IEntity
+    public function setter(string $prop, $value): self
     {
-        $this->{$this->realProp($prop)} = $value;
+        $this->data[$this->realProp($prop)] = $value;
 
         return $this;
     }
 
     /**
-     * getter.
+     * Getter.
      *
      * @return mixed
      */
     public function getter(string $prop)
     {
-        return $this->{$this->realProp($prop)};
+        return $this->data[$this->realProp($prop)] ?? null;
     }
 
     /**
-     * set database connect.
+     * Set database connect.
      *
      * @param mixed $connect
      */
     public static function withConnect($connect): void
     {
-        static::$leevelConnect = $connect;
+        static::$connect = $connect;
     }
 
     /**
-     * get database connect.
+     * Get database connect.
      */
     public static function connect()
     {
-        return static::$leevelConnect;
+        return static::$connect;
     }
 }
