f4d7cefb7dc0107e2bbc659a60a791a95a43fde1
XiangMin Liu <635750556@qq.com>
2019-12-21 15:20:18 +0800

    fix(any): update new entity

--- application/app/Domain/Entity/Test.php
+++ application/app/Domain/Entity/Test.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace App\Domain\Entity;
 
 use Leevel\Database\Ddd\Entity;
-use Leevel\Database\Ddd\IEntity;
 
 /**
  * Test.
@@ -23,37 +22,61 @@ use Leevel\Database\Ddd\IEntity;
 class Test extends Entity
 {
     /**
-     * database table.
+     * Database table.
      *
-     * @string
+     * @var string
      */
     const TABLE = 'test';
 
     /**
-     * primary key.
+     * Primary key.
      *
-     * @string|null
+     * @var string
      */
     const ID = 'id';
 
     /**
-     * auto increment.
+     * Auto increment.
      *
-     * @string|null
+     * @var null
      */
-    const AUTO = 'id';
+    const AUTO = null;
 
     /**
-     * entity struct.
+     * Entity struct.
      *
-     * @array
+     * - id
+     *                   comment: 'ID', type: 'int(11) unsigned', null: false,
+     *                   key: 'PRI', default: NULL, extra: 'auto_increment'
+     * - name
+     *                   comment: '测试名', type: 'varchar(200)', null: false,
+     *                   key: '', default: '', extra: ''
+     * - create_at
+     *                   comment: '创建时间', type: 'datetime', null: false,
+     *                   key: '', default: 'CURRENT_TIMESTAMP', extra: ''
+     * - update_at
+     *                   comment: '更新时间', type: 'datetime', null: false,
+     *                   key: '', default: 'CURRENT_TIMESTAMP', extra: 'on update CURRENT_TIMESTAMP'
+     * - delete_at
+     *                   comment: '删除时间 0=未删除;大于0=删除时间;', type: 'bigint(20) unsigned', null: false,
+     *                   key: 'MUL', default: '0', extra: ''
+     * - create_account
+     *                   comment: '创建账号', type: 'int(11) unsigned', null: false,
+     *                   key: '', default: '0', extra: ''
+     * - update_account
+     *                   comment: '更新账号', type: 'int(11) unsigned', null: false,
+     *                   key: '', default: '0', extra: ''
+     *
+     * @var array
      */
     const STRUCT = [
         'id' => [
             self::READONLY => true,
         ],
-        'name'      => [],
-        'create_at' => [],
+        'name' => [
+        ],
+        'create_at' => [
+        ],
         'update_at' => [
             self::SHOW_PROP_BLACK => true,
         ],
@@ -69,93 +92,63 @@ class Test extends Entity
     ];
 
     /**
-     * soft delete column.
+     * Soft delete column.
      *
      * @var string
      */
     const DELETE_AT = 'delete_at';
 
     /**
-     * database connect.
+     * Prop data.
+     *
+     * @var array
+     */
+    private array $data = [];
+
+    /**
+     * Database connect.
      *
      * @var mixed
      */
-    private static $leevelConnect;
+    private static $connect;
 
     /**
-     * id.
-     *
-     * @var int
-     */
-    private $id;
-
-    /**
-     * name.
-     *
-     * @var string
-     */
-    private $name;
-
-    /**
-     * 删除时间 0=未删除;大于0=删除时间;.
-     *
-     * @var int
-     */
-    private $deleteAt;
-
-    /**
-     * 创建账号.
-     *
-     * @var int
-     */
-    private $createAccount;
-
-    /**
-     * 更新账号.
-     *
-     * @var int
-     */
-    private $updateAccount;
-
-    /**
-     * setter.
+     * Setter.
      *
      * @param mixed $value
-     *
-     * @return $this
      */
-    public function setter(string $prop, $value): IEntity
+    public function setter(string $prop, $value): self
     {
-        $this->{$this->realProp($prop)} = $value;
+        $this->data[$this->realProp($prop)] = $value;
 
         return $this;
     }
 
     /**
-     * getter.
+     * Getter.
      *
      * @return mixed
      */
     public function getter(string $prop)
     {
-        return $this->{$this->realProp($prop)};
+        return $this->data[$this->realProp($prop)] ?? null;
     }
 
     /**
-     * set database connect.
+     * Set database connect.
      *
      * @param mixed $connect
      */
     public static function withConnect($connect): void
     {
-        static::$leevelConnect = $connect;
+        static::$connect = $connect;
     }
 
     /**
-     * get database connect.
+     * Get database connect.
      */
     public static function connect()
     {
-        return static::$leevelConnect;
+        return static::$connect;
     }
 }
