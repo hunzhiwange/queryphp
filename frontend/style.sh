13a1c3ba92f68608fd46f72f4bb48023aadea9c4
XiangMin Liu <635750556@qq.com>
2020-03-07 23:13:27 +0800

    chore(any): update style.sh

--- frontend/style.sh
+++ frontend/style.sh
@@ -4,4 +4,4 @@ set -e
 
 ./node_modules/.bin/gulp iview
 
-./node_modules/.bin/prettier --config .prettierrc.js --ignore-path .prettierignore --write vue.config.js babel.config.js .eslintrc.js gulpfile.js src/assets/css/global.css **/src/**
+./node_modules/.bin/prettier --config .prettierrc.js --ignore-path .prettierignore --write vue.config.js babel.config.js .eslintrc.js gulpfile.js **/src/**
