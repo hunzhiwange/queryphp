7e3b93440db9cfc3939348e674c9bab358e215f7
XiangMin Liu <635750556@qq.com>
2020-06-26 14:48:17 +0800

    fix(tests): fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,8 +29,7 @@ if (false === is_file($vendorDir.'/autoload.php')) {
 $composer = include $vendorDir.'/autoload.php';
 $composer->addPsr4('Tests\\', __DIR__);
 
-if (!class_exists(\PHPUnit\Framework\TestCase::class) ||
-    !class_exists(\Codedungeon\PHPUnitPrettyResultPrinter\Printer::class)) {
+if (!class_exists(\PHPUnit\Framework\TestCase::class)) {
     $e = 'If you execute command `composer dump-autoload --optimize --no-dev`,'.
     'then this will not be available.'.PHP_EOL.
     'PHPUnit and PHPStan belongs to development dependence and `composer dump-autoload --optimize` is ok.';
