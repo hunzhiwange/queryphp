e246938e29474fa25244665ea62320e2f8b02af5
XiangMin Liu <635750556@qq.com>
2020-11-28 16:16:10 +0800

    refactor: move swagger files to router/apis

--- router/apis/api.php
+++ router/apis/api.php
@@ -13,14 +13,14 @@ declare(strict_types=1);
  */
 
 /**
- * 路由.
+ * API 定义.
  *
  * - 路由相关主要由路由服务提供者进行设置。
  * - 大部分场景只需要使用基于路径的默认路由，不需要配置路由。
- * - 其他定制路由都是基于 OpenAPI 的路由。
+ * - 1.1.0-alpha.2 之前的版本，定制路由都是基于 OpenAPI 的路由，之后采用 PHP 8 注解实现。
  *
  * @see 路由服务提供者 \Common\Infra\ProviderRouter
- * @see 基于 OpenAPI 的路由 \App\App\Controller\Petstore\Store
+ * @see 基于 PHP 8 注解路由 \App\App\Controller\Petstore\Store
  *
  * @OA\Info(
  *     description="This is a sample Petstore server.  You can find
