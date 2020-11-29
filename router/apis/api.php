df5ab96c1200ed08875720ef51b785cb29a6e781
XiangMin Liu <635750556@qq.com>
2020-11-29 20:32:43 +0800

    refact: update swagger

--- router/apis/api.php
+++ router/apis/api.php
@@ -23,58 +23,12 @@ declare(strict_types=1);
  * @see 基于 PHP 8 注解路由 \App\App\Controller\Petstore\Store
  *
  * @OA\Info(
- *     description="This is a sample Petstore server.  You can find
- * out more about Swagger at
- * [http://swagger.io](http://swagger.io) or on
- * [irc.freenode.net, #swagger](http://swagger.io/irc/).",
+ *     description="The QueryPHP application Apis",
  *     version="1.0.0",
- *     title="Swagger Petstore",
- *     termsOfService="http://swagger.io/terms/",
- *     @OA\Contact(
- *         email="apiteam@swagger.io"
- *     ),
- *     @OA\License(
- *         name="Apache 2.0",
- *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
- *     )
+ *     title="QueryPHP Apis",
  * )
  */
 class _
-{
-    // 占位防止代码格式化工具将注释破坏
-}
-
-/**
- * @OA\Tag(
- *     name="pet",
- *     description="Everything about your Pets",
- *     @OA\ExternalDocumentation(
- *         description="Find out more",
- *         url="http://swagger.io"
- *     )
- * )
- * @OA\Tag(
- *     name="store",
- *     description="Access to Petstore orders",
- * )
- * @OA\Tag(
- *     name="user",
- *     description="Operations about user",
- *     @OA\ExternalDocumentation(
- *         description="Find out more about store",
- *         url="http://swagger.io"
- *     )
- * )
- * @OA\Server(
- *     description="SwaggerHUB API Mocking 1.0.0",
- *     url="https://virtserver.swaggerhub.com/swagger/Petstore"
- * )
- * @OA\ExternalDocumentation(
- *     description="Find out more about Swagger",
- *     url="http://swagger.io",
- * )
- */
-class __
 {
     // 占位防止代码格式化工具将注释破坏
 }
