2ae7305f9c38c9a3a43149871b362254b976ed9c
XiangMin Liu <635750556@qq.com>
2020-12-06 12:32:41 +0800

    style: code poem

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -2,35 +2,16 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the your app package.
- *
- * The PHP Application For Code Poem For You.
- * (c) 2018-2099 http://yourdomian.com All rights reserved.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+// phpstan 静态检查启动文件.
+include __DIR__.'/tests/bootstrap.php';
 
-/**
- * phpstan 静态检查启动文件.
- */
-include __DIR__.'/tests/bootstrap.php'; /** @codeCoverageIgnore */
-
-/**
- * 导入助手函数.
- *
- * @codeCoverageIgnoreStart
- */
+// 导入助手函数.
 $fnDirs = [
     __DIR__.'/common/Infra/Helper',
 ];
-
 foreach ($fnDirs as $dir) {
     $files = glob($dir.'/*.php');
-
     foreach ($files as $file) {
         include_once $file;
     }
 }
-// @codeCoverageIgnoreEnd
