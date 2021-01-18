6460f69f87d2bd92a9555b977a45ffe8f3ead45e
XiangMin Liu <635750556@qq.com>
2021-01-18 22:44:21 +0800

    fix: Fix get token

--- apps/app/Infra/Provider/App.php
+++ apps/app/Infra/Provider/App.php
@@ -8,6 +8,7 @@ use Admin\Infra\Permission;
 use Admin\Infra\PermissionCache;
 use Leevel\Di\IContainer;
 use Leevel\Di\Provider;
+use Leevel\Http\Request;
 
 /**
  * 应用服务提供者.
@@ -48,7 +49,9 @@ class App extends Provider
         $this->container->singleton('permission', function (IContainer $container): Permission {
             /** @var \Leevel\Http\Request $request */
             $request = $container->make('request');
-            $token = $request->query->get('token', '');
+            $token = Request::METHOD_GET === $request->getMethod()?
+                        $request->query->get('token', '') : 
+                        $request->request->get('token', '');
 
             return new Permission($container->make(PermissionCache::class), $token);
         });
