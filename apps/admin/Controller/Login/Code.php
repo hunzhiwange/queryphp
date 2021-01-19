c867a43498ab7a06c5e07f1636ae333b2f73f27b
XiangMin Liu <635750556@qq.com>
2021-01-19 21:02:53 +0800

    refactor: Refact code feature

--- apps/admin/Controller/Login/Code.php
+++ apps/admin/Controller/Login/Code.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Admin\Controller\Login;
 
 use Admin\Controller\Support\Controller;
-use Admin\Service\Login\Code as Service;
+use App\Domain\Service\Login\Code as Service;
+use App\Domain\Service\Login\CodeParams;
+
 use function App\Infra\Helper\force_close_debug;
 use Leevel\Http\Request;
 use Leevel\Http\Response;
@@ -21,7 +23,8 @@ class Code
 
     public function handle(Request $request, Service $service): Response
     {
-        $code = $service->handle($this->input($request));
+        $params = new CodeParams($this->input($request));
+        $code = $service->handle($params);
         $this->forceCloseDebug();
 
         return new Response($code, 200, ['Content-type' => 'image/png']);
@@ -36,6 +39,8 @@ class Code
 
     /**
      * 关闭调试模式.
+     * 
+     * - 调试代码会导致验证码图像无法生成
      */
     private function forceCloseDebug(): void
     {
