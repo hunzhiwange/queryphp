3c35b4ac5a9b856117cb316fce5d6ed0b26c1687
XiangMin Liu <635750556@qq.com>
2021-01-19 21:00:27 +0800

    refactor: Add comment to App\Service\Demo\Demo

--- apps/app/Service/Demo/Demo.php
+++ apps/app/Service/Demo/Demo.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Service\Demo;
 
+/**
+ * 应用服务层.
+ * 
+ * - 控制器应该足够精简，如果控制器存在一些逻辑处理可以移动到应用服务层
+ * - 如果控制器本身太过于简单，那么此时是不需要应用服务层的
+ */
 class Demo
 {
     public function handle(): array
