ab6e48849869d713702abdeb609ffc0bff8818b5
XiangMin Liu <635750556@qq.com>
2021-01-21 00:11:44 +0800

    refactor: Code close debug split to trait

--- apps/admin/Controller/Login/Code.php
+++ apps/admin/Controller/Login/Code.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Admin\Controller\Login;
 
+use Admin\Controller\Support\CloseDebug;
 use Admin\Controller\Support\Controller;
 use App\Domain\Service\Login\Code as Service;
 use App\Domain\Service\Login\CodeParams;
-
-use function App\Infra\Helper\force_close_debug;
 use Leevel\Http\Request;
 use Leevel\Http\Response;
 
@@ -20,12 +19,13 @@ use Leevel\Http\Response;
 class Code
 {
     use Controller;
+    use CloseDebug;
 
     public function handle(Request $request, Service $service): Response
     {
+        $this->closeDebug();
         $params = new CodeParams($this->input($request));
         $code = $service->handle($params);
-        $this->forceCloseDebug();
 
         return new Response($code, 200, ['Content-type' => 'image/png']);
     }
@@ -35,15 +35,5 @@ class Code
         return [
             'id' => $request->query->get('id'),
         ];
-    }
-
-    /**
-     * 关闭调试模式.
-     * 
-     * - 调试代码会导致验证码图像无法生成
-     */
-    private function forceCloseDebug(): void
-    {
-        func(fn () => force_close_debug());
     }
 }
