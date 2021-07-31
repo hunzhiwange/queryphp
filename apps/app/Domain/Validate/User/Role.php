d861f473f00fd5e0d3481137a42fb2b827cb2ab7
XiangMin Liu <635750556@qq.com>
2021-07-31 10:49:34 +0800

    feat: Add entity validator

---
+++ apps/app/Domain/Validate/User/Role.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Validate\User;
+
+use App\Domain\Entity\User\Role as UserRole;
+use App\Domain\Validate\IValidator;
+
+/**
+ * 角色验证.
+ */
+class Role implements IValidator
+{
+    public function __construct(private string $uniqueRule)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function scenes(): array
+    {
+        return [
+            'update' => [
+                'id',
+                'name',
+                'num',
+                'status',
+            ],
+            'store' => [
+                'name',
+                'num',
+                'status',
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function names(): array
+    {
+        return UserRole::columnNames();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function messages(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|type:int|gt:0',
+            'name' => ['required|chinese_alpha_num|max_length:50', $this->uniqueRule],
+            'num'  => ['required|alpha_dash', $this->uniqueRule],
+            'status' => [
+                ['in', UserRole::values('status')],
+            ],
+        ];
+    }
+}
---
+++ apps/app/Domain/Validate/User/User.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Validate\User;
+
+use App\Domain\Entity\User\User as UserUser;
+use App\Domain\Validate\IValidator;
+use Leevel\Validate\IValidator as ValidateIValidator;
+
+/**
+ * 用户验证.
+ */
+class User implements IValidator
+{
+    public function __construct(private string $uniqueRule)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function scenes(): array
+    {
+        return [
+            'update' => [
+                'id',
+                ':num' => ValidateIValidator::OPTIONAL,
+                ':password' => ValidateIValidator::OPTIONAL,
+                ':status'  => ValidateIValidator::OPTIONAL,
+                'email',
+                'mobile',
+            ],
+            'store' => [
+                'name',
+                'num',
+                'password',
+                'status',
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function names(): array
+    {
+        return UserUser::columnNames();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function messages(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|type:int|gt:0',
+            'name' => ['required|chinese_alpha_num|max_length:50', $this->uniqueRule],
+            'num'  => ['required|alpha_dash', $this->uniqueRule],
+            'password' => 'required|min_length:6,max_length:30',
+            'status' => [
+                ['in', UserUser::values('status')],
+            ],
+            'email'  => 'email|'.ValidateIValidator::OPTIONAL,
+            'mobile' => 'mobile|'.ValidateIValidator::OPTIONAL,
+        ];
+    }
+}
