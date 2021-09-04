306663b49ee3bd6446f051bc854107bb6fca23e5
XiangMin Liu <635750556@qq.com>
2021-09-04 16:04:58 +0800

    feat: Add project show

---
+++ apps/app/Domain/Service/Project/Project/ShowParams.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Service\Project\Project;
+
+use Leevel\Support\Dto;
+
+/**
+ * 项目查询参数.
+ */
+class ShowParams extends Dto
+{
+    public string $num;
+}
