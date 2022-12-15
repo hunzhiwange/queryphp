3b692b439832c36483a7af3734578566c69ad356
XiangMin Liu <635750556@qq.com>
2022-12-15 16:32:30 +0800

    优化异常

--- assets/exceptions/layout_json.php
+++ assets/exceptions/layout_json.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 return [
     'error' => [
         'title' => $title,
-        'status_code' => $status_code ?? 500,
         'code' => $code,
         'message' => $message,
     ],
