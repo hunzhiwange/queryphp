f5280d451d7ad583234a4899cd080e1847734302
XiangMin Liu <635750556@qq.com>
2022-12-25 15:22:14 +0800

    分类参数优化

--- apps/app/Domain/Service/Project/ProjectTag/UpdateParams.php
+++ apps/app/Domain/Service/Project/ProjectTag/UpdateParams.php
@@ -4,14 +4,35 @@ declare(strict_types=1);
 
 namespace App\Domain\Service\Project\ProjectTag;
 
-use Leevel\Support\Dto;
+use App\Domain\Entity\Project\ProjectTag;
+use App\Domain\Service\Support\UpdateParams as CommonUpdateParams;
+use App\Domain\Validate\Project\ProjectTag as ProjectTagValidate;
+use Leevel\Validate\UniqueRule;
 
 /**
  * 项目标签更新参数.
  */
-class UpdateParams extends Dto
+class UpdateParams extends CommonUpdateParams
 {
     use BaseStoreUpdateParams;
 
     public int $id;
+
+    protected string $validatorClass = ProjectTagValidate::class;
+
+    protected string $entityClass = ProjectTag::class;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function validatorArgs(): array
+    {
+        $uniqueRule = UniqueRule::rule(
+            $this->entityClass,
+            exceptId:$this->id,
+            additional:['project_id' => $this->projectId]
+        );
+
+        return [$uniqueRule];
+    }
 }
