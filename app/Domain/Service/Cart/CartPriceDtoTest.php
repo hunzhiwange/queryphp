1c5de580348eb4d93b9df10da80b099cb7ffc14b
XiangMin Liu <635750556@qq.com>
2023-02-07 01:43:25 +0800

    324234

--- app/Domain/Service/Cart/CartPriceDtoTest.php
+++ app/Domain/Service/Cart/CartPriceDtoTest.php
@@ -18,26 +18,26 @@ final class CartPriceDtoTest extends TestCase
         $foo = new CartPriceDto();
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 0,
-    "purchase_total_price": 0,
-    "should_special": false,
-    "special_total_price": 0,
-    "special_favorable_total_price": 0,
-    "coupon_favorable_total_price": 0,
-    "rebate_favorable_total_price": 0,
-    "integral_favorable_total_price": 0,
-    "freight": 0,
-    "invoice_type": 0,
-    "invoice_price": 0,
-    "invoice_tax": 0,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 0,
+                "purchase_total_price": 0,
+                "should_special": false,
+                "special_total_price": 0,
+                "special_favorable_total_price": 0,
+                "coupon_favorable_total_price": 0,
+                "rebate_favorable_total_price": 0,
+                "integral_favorable_total_price": 0,
+                "freight": 0,
+                "invoice_type": 0,
+                "invoice_price": 0,
+                "invoice_tax": 0,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 
@@ -48,26 +48,26 @@ eot;
         ]);
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 5,
-    "purchase_total_price": 5,
-    "should_special": false,
-    "special_total_price": 0,
-    "special_favorable_total_price": 0,
-    "coupon_favorable_total_price": 0,
-    "rebate_favorable_total_price": 0,
-    "integral_favorable_total_price": 0,
-    "freight": 0,
-    "invoice_type": 0,
-    "invoice_price": 0,
-    "invoice_tax": 0,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 5,
+                "purchase_total_price": 5,
+                "should_special": false,
+                "special_total_price": 0,
+                "special_favorable_total_price": 0,
+                "coupon_favorable_total_price": 0,
+                "rebate_favorable_total_price": 0,
+                "integral_favorable_total_price": 0,
+                "freight": 0,
+                "invoice_type": 0,
+                "invoice_price": 0,
+                "invoice_tax": 0,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 
@@ -79,26 +79,26 @@ eot;
         ]);
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 6,
-    "purchase_total_price": 5,
-    "should_special": false,
-    "special_total_price": 0,
-    "special_favorable_total_price": 0,
-    "coupon_favorable_total_price": 0,
-    "rebate_favorable_total_price": 0,
-    "integral_favorable_total_price": 0,
-    "freight": 1,
-    "invoice_type": 0,
-    "invoice_price": 0,
-    "invoice_tax": 0,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 6,
+                "purchase_total_price": 5,
+                "should_special": false,
+                "special_total_price": 0,
+                "special_favorable_total_price": 0,
+                "coupon_favorable_total_price": 0,
+                "rebate_favorable_total_price": 0,
+                "integral_favorable_total_price": 0,
+                "freight": 1,
+                "invoice_type": 0,
+                "invoice_price": 0,
+                "invoice_tax": 0,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 
@@ -112,26 +112,26 @@ eot;
         ]);
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 6.15,
-    "purchase_total_price": 5,
-    "should_special": false,
-    "special_total_price": 0,
-    "special_favorable_total_price": 0,
-    "coupon_favorable_total_price": 0,
-    "rebate_favorable_total_price": 0,
-    "integral_favorable_total_price": 0,
-    "freight": 1,
-    "invoice_type": 1,
-    "invoice_price": 0.15,
-    "invoice_tax": 3,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 6.15,
+                "purchase_total_price": 5,
+                "should_special": false,
+                "special_total_price": 0,
+                "special_favorable_total_price": 0,
+                "coupon_favorable_total_price": 0,
+                "rebate_favorable_total_price": 0,
+                "integral_favorable_total_price": 0,
+                "freight": 1,
+                "invoice_type": 1,
+                "invoice_price": 0.15,
+                "invoice_tax": 3,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 
@@ -147,26 +147,26 @@ eot;
         ]);
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 3,
-    "purchase_total_price": 5,
-    "should_special": true,
-    "special_total_price": 2,
-    "special_favorable_total_price": 3.15,
-    "coupon_favorable_total_price": 0,
-    "rebate_favorable_total_price": 0,
-    "integral_favorable_total_price": 0,
-    "freight": 1,
-    "invoice_type": 1,
-    "invoice_price": 0.15,
-    "invoice_tax": 3,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 3,
+                "purchase_total_price": 5,
+                "should_special": true,
+                "special_total_price": 2,
+                "special_favorable_total_price": 3.15,
+                "coupon_favorable_total_price": 0,
+                "rebate_favorable_total_price": 0,
+                "integral_favorable_total_price": 0,
+                "freight": 1,
+                "invoice_type": 1,
+                "invoice_price": 0.15,
+                "invoice_tax": 3,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 
@@ -180,26 +180,26 @@ eot;
         ]);
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 4,
-    "purchase_total_price": 7,
-    "should_special": false,
-    "special_total_price": 0,
-    "special_favorable_total_price": 0,
-    "coupon_favorable_total_price": 1,
-    "rebate_favorable_total_price": 1,
-    "integral_favorable_total_price": 1,
-    "freight": 0,
-    "invoice_type": 0,
-    "invoice_price": 0,
-    "invoice_tax": 0,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 4,
+                "purchase_total_price": 7,
+                "should_special": false,
+                "special_total_price": 0,
+                "special_favorable_total_price": 0,
+                "coupon_favorable_total_price": 1,
+                "rebate_favorable_total_price": 1,
+                "integral_favorable_total_price": 1,
+                "freight": 0,
+                "invoice_type": 0,
+                "invoice_price": 0,
+                "invoice_tax": 0,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 
@@ -216,26 +216,26 @@ eot;
         ]);
         $foo->calculateFinalTotalPrice();
         $json = <<<'eot'
-{
-    "final_total_price": 5.21,
-    "purchase_total_price": 7,
-    "should_special": false,
-    "special_total_price": 0,
-    "special_favorable_total_price": 0,
-    "coupon_favorable_total_price": 1,
-    "rebate_favorable_total_price": 1,
-    "integral_favorable_total_price": 1,
-    "freight": 1,
-    "invoice_type": 1,
-    "invoice_price": 0.21,
-    "invoice_tax": 3,
-    "invoice_tax_all": [
-        0,
-        3,
-        5
-    ]
-}
-eot;
+            {
+                "final_total_price": 5.21,
+                "purchase_total_price": 7,
+                "should_special": false,
+                "special_total_price": 0,
+                "special_favorable_total_price": 0,
+                "coupon_favorable_total_price": 1,
+                "rebate_favorable_total_price": 1,
+                "integral_favorable_total_price": 1,
+                "freight": 1,
+                "invoice_type": 1,
+                "invoice_price": 0.21,
+                "invoice_tax": 3,
+                "invoice_tax_all": [
+                    0,
+                    3,
+                    5
+                ]
+            }
+            eot;
         static::assertSame($json, $this->varJson($foo->toArray()));
     }
 }
