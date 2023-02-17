d0e08fb3fc44b22a684d2b21737b2e162868ee93
XiangMin Liu <635750556@qq.com>
2023-02-17 14:00:13 +0800

    更新模型

--- app/Domain/Entity/User/PermissionResource.php
+++ app/Domain/Entity/User/PermissionResource.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Domain\Entity\User;
 
 use Leevel\Database\Ddd\Entity;
+use Leevel\Database\Ddd\Struct;
 
 /**
  * 权限资源关联.
  */
-class PermissionResource extends Entity
+final class PermissionResource extends Entity
 {
     /**
      * Database table.
@@ -27,72 +28,92 @@ class PermissionResource extends Entity
     public const AUTO = 'id';
 
     /**
-     * Entity struct.
-     *
-     * - id
-     *                   comment: ID  type: bigint(20) unsigned  null: false
-     *                   key: PRI  default: null  extra: auto_increment
-     * - permission_id
-     *                   comment: 权限 ID  type: bigint(20) unsigned  null: false
-     *                   key: MUL  default: 0  extra:
-     * - resource_id
-     *                   comment: 资源 ID  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - create_at
-     *                   comment: 创建时间  type: datetime  null: false
-     *                   key:   default: CURRENT_TIMESTAMP  extra:
-     * - update_at
-     *                   comment: 更新时间  type: datetime  null: false
-     *                   key:   default: CURRENT_TIMESTAMP  extra: on update CURRENT_TIMESTAMP
-     * - delete_at
-     *                   comment: 删除时间 0=未删除;大于0=删除时间;  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - create_account
-     *                   comment: 创建账号  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - update_account
-     *                   comment: 更新账号  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - version
-     *                   comment: 操作版本号  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     */
-    public const STRUCT = [
-        'id' => [
-            self::COLUMN_NAME => 'ID',
-            self::READONLY => true,
-        ],
-        'permission_id' => [
-            self::COLUMN_NAME => '权限 ID',
-        ],
-        'resource_id' => [
-            self::COLUMN_NAME => '资源 ID',
-        ],
-        'create_at' => [
-            self::COLUMN_NAME => '创建时间',
-        ],
-        'update_at' => [
-            self::COLUMN_NAME => '更新时间',
-        ],
-        'delete_at' => [
-            self::COLUMN_NAME => '删除时间 0=未删除;大于0=删除时间;',
-            self::SHOW_PROP_BLACK => true,
-        ],
-        'create_account' => [
-            self::COLUMN_NAME => '创建账号',
-            self::SHOW_PROP_BLACK => true,
-        ],
-        'update_account' => [
-            self::COLUMN_NAME => '更新账号',
-            self::SHOW_PROP_BLACK => true,
-        ],
-        'version' => [
-            self::COLUMN_NAME => '操作版本号',
-        ],
-    ]; // END STRUCT
-
-    /**
      * Soft delete column.
      */
     public const DELETE_AT = 'delete_at';
+
+    #[Struct([
+        self::COLUMN_NAME => 'ID',
+        self::READONLY => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $id = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '权限 ID',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $permissionId = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '资源 ID',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $resourceId = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '创建时间',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'datetime',
+            'type_length' => null,
+        ],
+    ])]
+    protected ?string $createAt = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '更新时间',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'datetime',
+            'type_length' => null,
+        ],
+    ])]
+    protected ?string $updateAt = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '删除时间 0=未删除;大于0=删除时间;',
+        self::SHOW_PROP_BLACK => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $deleteAt = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '创建账号',
+        self::SHOW_PROP_BLACK => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $createAccount = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '更新账号',
+        self::SHOW_PROP_BLACK => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $updateAccount = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '操作版本号',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $version = null;
 }
--- app/Domain/Entity/User/User.php
+++ app/Domain/Entity/User/User.php
@@ -6,12 +6,14 @@ namespace App\Domain\Entity\User;
 
 use App\Infra\Repository\User\User as RepositoryUser;
 use Leevel\Database\Ddd\Entity;
+use Leevel\Database\Ddd\EntityCollection;
 use Leevel\Database\Ddd\Relation\ManyMany;
+use Leevel\Database\Ddd\Struct;
 
 /**
  * 用户.
  */
-class User extends Entity
+final class User extends Entity
 {
     /**
      * Database table.
@@ -29,105 +31,6 @@ class User extends Entity
     public const AUTO = 'id';
 
     /**
-     * Entity struct.
-     *
-     * - id
-     *                   comment: ID  type: bigint(20) unsigned  null: false
-     *                   key: PRI  default: null  extra: auto_increment
-     * - name
-     *                   comment: 用户名字  type: varchar(64)  null: false
-     *                   key: MUL  default:   extra:
-     * - num
-     *                   comment: 编号  type: varchar(64)  null: false
-     *                   key: MUL  default:   extra:
-     * - password
-     *                   comment: 密码  type: varchar(255)  null: false
-     *                   key:   default:   extra:
-     * - email
-     *                   comment: Email  type: varchar(100)  null: false
-     *                   key:   default:   extra:
-     * - mobile
-     *                   comment: 手机  type: char(11)  null: false
-     *                   key:   default:   extra:
-     * - status
-     *                   comment: 状态 0=禁用;1=启用;  type: tinyint(1)  null: false
-     *                   key:   default: 1  extra:
-     * - create_at
-     *                   comment: 创建时间  type: datetime  null: false
-     *                   key:   default: CURRENT_TIMESTAMP  extra:
-     * - update_at
-     *                   comment: 更新时间  type: datetime  null: false
-     *                   key:   default: CURRENT_TIMESTAMP  extra: on update CURRENT_TIMESTAMP
-     * - delete_at
-     *                   comment: 删除时间 0=未删除;大于0=删除时间;  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - create_account
-     *                   comment: 创建账号  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - update_account
-     *                   comment: 更新账号  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     * - version
-     *                   comment: 操作版本号  type: bigint(20) unsigned  null: false
-     *                   key:   default: 0  extra:
-     */
-    public const STRUCT = [
-        'id' => [
-            self::COLUMN_NAME => 'ID',
-            self::READONLY => true,
-        ],
-        'name' => [
-            self::COLUMN_NAME => '用户名字',
-        ],
-        'num' => [
-            self::COLUMN_NAME => '编号',
-        ],
-        'password' => [
-            self::COLUMN_NAME => '密码',
-        ],
-        'email' => [
-            self::COLUMN_NAME => 'Email',
-        ],
-        'mobile' => [
-            self::COLUMN_NAME => '手机',
-        ],
-        'status' => [
-            self::COLUMN_NAME => '状态 0=禁用;1=启用;',
-            self::ENUM_CLASS => UserStatusEnum::class,
-        ],
-        'create_at' => [
-            self::COLUMN_NAME => '创建时间',
-        ],
-        'update_at' => [
-            self::COLUMN_NAME => '更新时间',
-        ],
-        'delete_at' => [
-            self::COLUMN_NAME => '删除时间 0=未删除;大于0=删除时间;',
-            self::SHOW_PROP_BLACK => true,
-        ],
-        'create_account' => [
-            self::COLUMN_NAME => '创建账号',
-            self::SHOW_PROP_BLACK => true,
-        ],
-        'update_account' => [
-            self::COLUMN_NAME => '更新账号',
-            self::SHOW_PROP_BLACK => true,
-        ],
-        'version' => [
-            self::COLUMN_NAME => '操作版本号',
-        ],
-        'role' => [
-            self::MANY_MANY => Role::class,
-            self::MIDDLE_ENTITY => UserRole::class,
-            self::SOURCE_KEY => 'id',
-            self::TARGET_KEY => 'id',
-            self::MIDDLE_SOURCE_KEY => 'user_id',
-            self::MIDDLE_TARGET_KEY => 'role_id',
-            self::RELATION_SCOPE => 'role',
-        ],
-    ]; // END STRUCT
-
-    /**
      * Soft delete column.
      */
     public const DELETE_AT = 'delete_at';
@@ -136,6 +39,139 @@ class User extends Entity
      * 仓储.
      */
     public const REPOSITORY = RepositoryUser::class;
+
+    #[Struct([
+        self::COLUMN_NAME => 'ID',
+        self::READONLY => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $id = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '用户名字',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'varchar',
+            'type_length' => 64,
+        ],
+    ])]
+    protected ?string $name = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '编号',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'varchar',
+            'type_length' => 64,
+        ],
+    ])]
+    protected ?string $num = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '密码',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'varchar',
+            'type_length' => 255,
+        ],
+    ])]
+    protected ?string $password = null;
+
+    #[Struct([
+        self::COLUMN_NAME => 'Email',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'varchar',
+            'type_length' => 100,
+        ],
+    ])]
+    protected ?string $email = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '手机',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'char',
+            'type_length' => 11,
+        ],
+    ])]
+    protected ?string $mobile = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '状态 0=禁用;1=启用;',
+        self::ENUM_CLASS => UserStatusEnum::class,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'tinyint',
+            'type_length' => 1,
+        ],
+    ])]
+    protected ?int $status = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '创建时间',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'datetime',
+            'type_length' => null,
+        ],
+    ])]
+    protected ?string $createAt = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '更新时间',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'datetime',
+            'type_length' => null,
+        ],
+    ])]
+    protected ?string $updateAt = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '删除时间 0=未删除;大于0=删除时间;',
+        self::SHOW_PROP_BLACK => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $deleteAt = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '创建账号',
+        self::SHOW_PROP_BLACK => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $createAccount = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '更新账号',
+        self::SHOW_PROP_BLACK => true,
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $updateAccount = null;
+
+    #[Struct([
+        self::COLUMN_NAME => '操作版本号',
+        self::COLUMN_STRUCT => [
+            'type_name' => 'bigint',
+            'type_length' => 20,
+        ],
+    ])]
+    protected ?int $version = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'role',
+    ])]
+    protected ?EntityCollection $role = null;
 
     public static function repository(?Entity $entity = null): RepositoryUser
     {
