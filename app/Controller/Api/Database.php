cf9c76810a67672c4974e894597de27628112f9d
XiangMin Liu <635750556@qq.com>
2023-02-17 22:46:35 +0800

    删除调试代码

--- app/Controller/Api/Database.php
+++ app/Controller/Api/Database.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
-use App\Domain\Entity\Demo\Test;
 use Leevel\Database\Proxy\Db;
 
 /**
@@ -19,16 +18,6 @@ class Database
      */
     public function handle(): array
     {
-        echo 11;
-        // $test = new Test();
-        /** @var Test $demo */
-        $demo = Test::select()->findEntity(1);
-        //   print_r($demo);
-        //  $demo->createAt = 'xxxxx';
-        print_r($demo);
-        // $demo->fields();
-        exit;
-
         return ['count' => Db::table('test')->findCount()];
     }
 }
