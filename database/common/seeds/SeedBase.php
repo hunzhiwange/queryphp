d420fab5ea37a4f2006bd60a67257ca58351730c
XiangMin Liu <635750556@qq.com>
2024-05-06 12:05:52 +0800

    调整框架

---
+++ database/common/seeds/SeedBase.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace database\common\seeds;
+trait SeedBase
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function execute($sql): int
+    {
+        $sqlAll = array_filter(explode(';', $sql), fn($v) => '' !== trim($v));
+        if (!$sqlAll) {
+            return 0;
+        }
+
+        foreach ($sqlAll as $item) {
+            \Leevel\Database\Proxy\Db::connect('common')->execute($item);
+        }
+
+        return 0;
+    }
+}
