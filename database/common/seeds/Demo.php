91cb37ce96e09133d3ed833b2472d3d24e5f1438
Xiangmin Liu <635750556@qq.com>
2024-12-01 13:15:30 +0800

    2.0

---
+++ database/common/seeds/Demo.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Leevel\Database\Migrations\Seed;
+
+class ClientStarDiscountSeedClear extends Seed
+{
+    protected function sql(): string
+    {
+        return <<<'EOT'
+EOT;
+    }
+}
