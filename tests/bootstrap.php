351e8d9ad7c41f8a5b1b2fc6d2d2595ea978b25f
xiangmin liu <635750556@qq.com>
2026-06-02 22:35:44 +0800

    fix: fix for new version

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,8 +27,6 @@ $vendorDir = __DIR__.'/../vendor';
 
 require_once __DIR__.'/function.php';
 
-require_once __DIR__.'/Redis.php';
-
 if (false === is_file($vendorDir.'/autoload.php')) {
     throw new Exception('You must set up the app dependencies, run the following commands:
         wget http://getcomposer.org/composer.phar
